612c8dd5f1ecd4d8880e8fdb1e3540b4172493c7
Labyrintos <Labyrintos71@gmail.com>
2023-11-08 20:27:32 +0900

    파일 복사 및 리스트 입출력 기능 추가

--- src/main/kotlin/file/FileIO.kt
+++ src/main/kotlin/file/FileIO.kt
@@ -2,6 +2,7 @@ package file
 
 import java.io.File
 import java.io.InputStream
+import java.util.Arrays
 
 class FileIO {
     fun writeByInputStream(inputStream: InputStream, filePath: String) {
@@ -12,4 +13,20 @@ class FileIO {
             inputStream.copyTo(fileOutput)
         }
     }
+
+    fun writeFileByTest(path: String, text: String) {
+        val file = File(path)
+        file.writeText(text)
+        //  file.appendText("hello")
+    }
+
+    fun printFileLists(path: String) {
+        val files = File(path).listFiles()        // 지정해 준 경로 내의 파일을 리스트로 생성
+        println(files.contentToString())
+    }
+
+    fun copyFile(originPath: String, copyPath: String) {
+        File(originPath).copyTo(File(copyPath))
+    }
+    
 }
