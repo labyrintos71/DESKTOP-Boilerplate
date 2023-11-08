bc4285fd5441d0df54ff058858b689eafcb6066b
Labyrintos <Labyrintos71@gmail.com>
2023-11-08 20:29:34 +0900

    파일 읽기 추가

--- src/main/kotlin/file/FileIO.kt
+++ src/main/kotlin/file/FileIO.kt
@@ -20,6 +20,18 @@ class FileIO {
         //  file.appendText("hello")
     }
 
+    fun readText(path: String) {
+        val text = File(path).readText()
+        println(text)
+    }
+
+    fun readFile(path: String) {
+        val bufferedReader = File(path).bufferedReader()
+        val lineList = mutableListOf<String>()
+        bufferedReader.useLines { lines -> lines.forEach { lineList.add(it) } }
+        lineList.forEach { println(it) }
+    }
+
     fun printFileLists(path: String) {
         val files = File(path).listFiles()        // 지정해 준 경로 내의 파일을 리스트로 생성
         println(files.contentToString())
@@ -28,5 +40,5 @@ class FileIO {
     fun copyFile(originPath: String, copyPath: String) {
         File(originPath).copyTo(File(copyPath))
     }
-    
+
 }
